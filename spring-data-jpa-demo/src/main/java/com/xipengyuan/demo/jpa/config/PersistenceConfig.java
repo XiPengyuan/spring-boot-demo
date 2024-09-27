ebf2ce1bea38ac312cb0916d1ee74cade4d68b0a
Peter Xi <Peter.Xi@disney.com>
2024-09-27 11:46:19 +0800

    移除@EnableTransactionManagement

--- spring-data-jpa-demo/src/main/java/com/xipengyuan/demo/jpa/config/PersistenceConfig.java
+++ spring-data-jpa-demo/src/main/java/com/xipengyuan/demo/jpa/config/PersistenceConfig.java
@@ -3,11 +3,9 @@ package com.xipengyuan.demo.jpa.config;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EntityScan("com.xipengyuan.demo.jpa.persistence.entity")
 @EnableJpaRepositories("com.xipengyuan.demo.jpa.persistence.repository")
-@EnableTransactionManagement
 public class PersistenceConfig {
 }
