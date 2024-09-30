f28f4bd7e289df54849b19277588c0024b88b2f9
Peter Xi <Peter.Xi@disney.com>
2024-09-30 13:33:55 +0800

    移除@EntityScan、@EnableJpaRepositories

--- spring-data-jpa-demo/src/main/java/com/xipengyuan/demo/jpa/config/PersistenceConfig.java
+++ spring-data-jpa-demo/src/main/java/com/xipengyuan/demo/jpa/config/PersistenceConfig.java
@@ -1,11 +1,13 @@
 package com.xipengyuan.demo.jpa.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EntityScan("com.xipengyuan.demo.jpa.persistence.entity")
-@EnableJpaRepositories("com.xipengyuan.demo.jpa.persistence.repository")
+/*
+ * 由于实体和Repositories都在本项目的默认auto-configuration package（com.xipengyuan.demo.jpa）下
+ * 下面的注解也可以选择不添加
+ */
+// @EntityScan("com.xipengyuan.demo.jpa.persistence.entity")
+// @EnableJpaRepositories("com.xipengyuan.demo.jpa.persistence.repository")
 public class PersistenceConfig {
 }
